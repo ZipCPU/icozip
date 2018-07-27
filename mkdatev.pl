dbc4408fac8306c3db6a7d33a1219488fb12f864
ZipCPU <dgisselq@ieee.org>
2018-07-26 22:39:58 -0400

    Fixed project name in mkdatev.pl

--- mkdatev.pl
+++ mkdatev.pl
@@ -51,7 +51,7 @@ print <<"EOM";
 //
 // Filename: 	builddate.v
 //
-// Project:	VideoZip, a ZipCPU SoC supporting video functionality
+// Project:	ICO Zip, iCE40 ZipCPU demonsrtation project
 //
 // Purpose:	This file records the date of the last build.  Running "make"
 //		in the main directory will create this file.  The `define found
@@ -76,7 +76,7 @@ print <<"EOM";
 // for more details.
 //
 // You should have received a copy of the GNU General Public License along
-// with this program.  (It's in the $doc directory, run make with no
+// with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
 // target there if the PDF file isn't present.)  If not, see
 // <http://www.gnu.org/licenses/> for a copy.
 //
